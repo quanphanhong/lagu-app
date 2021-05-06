d6e56b932c5b993be034dce685a7fc182ffa78fd
quanphanhong <18520239@gm.uit.edu.vn>
2021-05-06 21:16:14 +0700

    add Search Bar

---
+++ lagu_app/lib/friend_list.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:lagu_app/friend_search_bar.dart';
+import 'package:mvc_application/view.dart';
+
+class FriendList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => FriendListState();
+}
+
+class FriendListState extends StateMVC<FriendList> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Friend List',
+      home: Scaffold(
+        appBar: FriendSearchBar(),
+      ),
+    );
+  }
+}
